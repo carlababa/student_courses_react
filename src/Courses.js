90d51bc2c08cf82e12a981ef79ca6e5fe1ddcbec
Carla Babadopulos <cacababa@gmail.com>
2016-04-26 10:22:00 +0200

    setting first steps react

---
+++ src/Courses.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import jQuery from 'jquery';
+
+class Courses extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      courses: []
+    };
+  }
+
+  componentDidMount() {
+    jQuery.get("http://localhost:3000/courses.json", (function(data){
+      this.setState({
+        courses: data.courses,
+      });
+    }).bind(this));
+  }
+
+  render() {
+    let courses = this.state.courses.map(function(course) {
+      return <li key={course.id}><h1>{course.name}</h1><p>{course.description}</p></li>;
+    });
+
+    return (
+        <ul>
+          {courses}
+        </ul>
+    )
+  }
+}
+
+export default Courses;
